8ead372ecb42b6706d17b760cea4bc189e8f1b4c
yindonghui <1293366147@qq.com>
2018-08-07 09:01:49 +0800

    第三次修改

--- js/index.js
+++ js/index.js
@@ -11,3 +11,44 @@ $(".catalog").mouseout(function(){
 	$(".subcat").css("display","none");
 
 })
+
+
+
+
+
+
+
+
+
+//第二个
+$("#brand_ul li").mouseover(function(){
+	var index=$(this).index();
+	$(".brand_img").eq(index).css("display","block").siblings().css("display","none");
+})
+
+
+
+
+
+//第三个
+$(".brand_img_zi a").mouseover(function(){
+	$(this).next().animate({
+		bottom:0
+	},500)
+})
+$(".brand_img_zi a").mouseout(function(){
+	$(this).next().animate({
+		bottom:-21
+	},500)
+})
+
+$(window).scroll(function(){
+	var src=$(this).scrollTop();
+	if(src>500){
+		$(".fix_nav").css("display","block");
+	}else{
+		$(".fix_nav").css("display","none");
+	}
+})
+
+
